a038a044112cb0da8f10f7ddd418eb235cd6a8a8
newbietechlady <fumex9910@gmail.com>
2022-04-14 17:08:20 +0100

    update static root

--- slitherstravels/settings.py
+++ slitherstravels/settings.py
@@ -147,7 +147,7 @@ MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
