2682c356380effadaaddcb3f4951c4746cf1110c
newbietechlady <fumex9910@gmail.com>
2022-04-15 00:36:57 +0100

    added django-heroku

--- slitherstravels/settings.py
+++ slitherstravels/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,3 +168,5 @@ EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+django_heroku.settings(locals())
