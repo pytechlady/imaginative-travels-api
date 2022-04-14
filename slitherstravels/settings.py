0c285fab5301789cdb0381029a289f5a3f782a33
newbietechlady <fumex9910@gmail.com>
2022-04-14 23:20:07 +0100

    added procfile

--- slitherstravels/settings.py
+++ slitherstravels/settings.py
@@ -155,7 +155,7 @@ MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = 587
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
