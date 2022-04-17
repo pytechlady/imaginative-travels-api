89fb21d4ee9e4a045eed055dcceac6ab63a17ead
newbietechlady <fumex9910@gmail.com>
2022-04-18 00:33:15 +0100

    image break fix

--- travelteam/models.py
+++ travelteam/models.py
@@ -5,7 +5,7 @@ class Team(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     title = models.CharField(max_length=30)
-    image = models.CharField(max_length=255)
+    image = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
