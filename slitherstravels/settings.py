b2050cf0eba63db72cec9b9bc212ade50268408a
pytechlady <username@Usernames-MacBook-Air.local>
2023-06-16 16:00:40 +0100

    hide secret key

--- slitherstravels/settings.py
+++ slitherstravels/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sw$g-9&xe#^9egwyz+t59x$x$pc@&teca493wev4jju%&e0uo*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
