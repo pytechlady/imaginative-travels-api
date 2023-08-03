d80e103a129228101bb2b420fba849a3c02b5d57
pytechlady <username@Usernames-MacBook-Air.local>
2023-08-03 14:17:55 +0100

    change ordering

--- travelblog/pagination.py
+++ travelblog/pagination.py
@@ -5,3 +5,4 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 50
     page_query_param = 'p'
+    
--- travelblog/serializers.py
+++ travelblog/serializers.py
@@ -8,4 +8,5 @@ class BlogSerializer(serializers.ModelSerializer):
         fields = [
             'id','title', 'content', 'category', 'travelphoto', 'travelphoto_2', 'travelphoto_3', 'travelphoto_4', 'created_at']
         read_only_fields = ['id', 'created_at']
+        ordering = ['created_at']
         
